cddfeaeef9d830929bc11617cc671b2f7296f842
Timur Stupin <hache03@gmail.com>
2025-03-07 13:30:03 +0300

    comments to lab2 added

--- part-2/leading-zeros.s
+++ part-2/leading-zeros.s
@@ -1,38 +1,45 @@
     .data
 
-input_addr:      .word  0x80 
-output_addr:     .word  0x84
-mask:            .word  0x80000000
+input_addr:      .word  0x80            \ input address
+output_addr:     .word  0x84            \ output address
+mask:            .word  0x80000000      \ bitmask
 
     .text
 _start:
-    @p input_addr a! @
+    @p input_addr a! @                  \ dataStack.push(mem[mem[input_addr]])
 
-    count_leading_zeros
+    count_leading_zeros 
 
-    @p output_addr a! !
+    @p output_addr a! !                 \ mem[mem[output_addr]] <- dataStack.pop()
     halt
 
 count_leading_zeros:
-    dup if zero
-    lit 0
-    over
+    dup if zero                         \ if dataStack.top() == 0 then goto zero
+    
+    lit 0                               \ dataStack.push(0)
+    over                                \ swap(T, S)
 
 count_cycle:
-    dup
-    @p mask
-    and
-    if continue
-    end ;
+    dup                                 \ dataStack.push(dataStack.top())
+    
+    @p mask                             \ dataStack.push(mem[mask])
+    and                                 \ dataStack.push(dataStack.pop() & dataStack.pop())  
+    
+    if continue                         \ if dataStack.top() == 0 then goto continue
+    end ;                               \ else goto end  
+
 continue:
-    over lit 1 + over
-    @p mask
+    over lit 1 + over                   \ S += 1
+    
+    @p mask                             \ mem[mask] >> 1
     2/
     !p mask
-    count_cycle ;
+
+    count_cycle ;                       \ goto count_cycle
+
 end:
-    over ;
+    over ;                              \ swap(T, S); return
 
 zero:
-    lit 32 ;
+    lit 32 ;                            \ dataStack.push(32); return
 
